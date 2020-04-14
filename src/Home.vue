ab36660499af72584af35d0962c63049251148d3
BonneC <bonnegr33n@gmail.com>
2020-04-14 02:09:10 +0200

    bootstrap n navbar

---
+++ src/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="main-home-container">
+        <div>
+            <div class="row no-gutters min-h-fullscreen bg-white home-fullscreen">
+
+                <div class="col-md-6 col-lg-7 col-xl-8 d-none d-md-block bg-img home-bg" data-overlay="5">
+                    <div class="row h-100 pl-50">
+                        <div class="col-md-10 col-lg-8 align-self-end">
+                            <img src="./assets/logo.png" alt="..." style="max-height: 120px">
+                            <br><br><br>
+                            <h4 class="text-white">Travelman</h4>
+                            <p class="text-white">Credibly transition sticky users after backward-compatible web
+                                services.
+                                Compellingly strategize team building interfaces.</p>
+                            <br><br>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    .main-home-container {
+        position: relative;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import Navbar from './Navbar'
 
+// import router from './router'
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+Vue.component('app-navbar', Navbar)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App)
 }).$mount('#app')
