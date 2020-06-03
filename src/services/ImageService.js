7a4570fda9b859dd0ff1db8011a6d926664203cf
BonneC <bonnegr33n@gmail.com>
2020-06-03 02:44:17 +0200

    working docker

--- src/services/ImageService.js
+++ src/services/ImageService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: 'http://localhost:8000',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
